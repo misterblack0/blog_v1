56660c28d6d51743a1e403840360ef75df25e446
misterblack0 <misterblack0@protonmail.com>
2020-11-10 20:56:57 +0200

    first commit

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,11 +1,13 @@
-import React from "react"
-import {Link} from "gatsby"
-import ThemeChanger from "../components/themeChanger"
+import React from "react";
+import { Link } from "gatsby";
+import ThemeChanger from "../components/themeChanger";
 
 export default (props) => (
-  <nav className="navigation"> 
+  <nav className="navigation">
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
     <Link to="/contact">Contact</Link>
-    <ThemeChanger/>
+    <Link to="/404">404 Page</Link>
+    <ThemeChanger />
   </nav>
-  
-)
+);
