cc200f2d7e1ce9b1c326e059fa8e29ad4dcc0aca
misterblack0 <misterblack0@protonmail.com>
2020-11-11 17:14:52 +0200

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,30 @@
 import React from "react";
 import Helmet from "react-helmet";
 import Layout from "../components/layout";
+import { graphql } from "gatsby";
 
 const AboutPage = ({ data: { site } }) => {
   return (
     <Layout>
       <Helmet>
-        <title>About me — {site.siteMetadata.title}</title>
+        <title>About — {site.siteMetadata.title}</title>
+        <meta
+          name="description"
+          content={"About page of " + site.siteMetadata.description}
+        />
       </Helmet>
     </Layout>
   );
 };
 
 export default AboutPage;
+export const pageQuery = graphql`
+  query AboutPage {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`;
